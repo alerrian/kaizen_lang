dcedf13dc9b458540fcc87627a40b763fc9c2608
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-10-03 20:55:44 -0600

    Add tokens for lexer

---
+++ token/token.go
@@ -0,0 +1,50 @@
+package token
+
+type Type string
+
+const (
+	// Special Types
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers and Literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN    = '='
+	PLUS      = '+'
+	MINUS     = '-'
+	BANG      = '!'
+	ASTERISK  = '*'
+	SLASH     = '/'
+	LESSTHAN  = '<'
+	GRTRTHAN  = '>'
+	EQUAL     = "=="
+	NOT_EQUAL = "!="
+
+	// Delimiters
+	COMMA     = ','
+	SEMICOLON = ';'
+	LPAREN    = '('
+	RPAREN    = ')'
+	LCURL     = '{'
+	RCURL     = '}'
+	LBRACK    = '['
+	RBRACK    = ']'
+
+	// Keywords
+	FUNCTION = "DEF"
+	VAR      = "VAR"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+)
+
+type Token struct {
+	Type    Type
+	Literal string
+}
+
